51f2b61e0162c3c19fad1cb7965d8f4962100f93
OlegChernik <chernikoleg7@gmail.com>
2022-07-25 11:34:30 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,34 @@
+
+const swiper = new Swiper('.swiper', {
+
+    direction: 'horizontal',
+    loop: true,
+  
+
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+
+const burger = document.querySelector('.burger')
+const navList = document.querySelector('.nav-lists')
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active')
+  navList.classList.toggle('active')
+})
+
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+  burger.classList.remove('active')
+  navList.classList.remove('active')
+}))
