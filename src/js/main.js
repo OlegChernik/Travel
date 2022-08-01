eceb56c220c71e662863c965f679dca192c7d98d
OlegChernik <chernikoleg7@gmail.com>
2022-08-01 18:35:08 +0300

    all project

--- src/js/main.js
+++ src/js/main.js
@@ -20,15 +20,13 @@ const swiper = new Swiper('.swiper', {
   });
 
 
+  
 const burger = document.querySelector('.burger')
 const navList = document.querySelector('.nav-lists')
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('active')
   navList.classList.toggle('active')
+ 
 })
 
-document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
-  burger.classList.remove('active')
-  navList.classList.remove('active')
-}))
